a025b7526471b7c9fcbc6089cef27aa89bf23907
JoseVilaseca <42195682+JoseVilaseca@users.noreply.github.com>
2020-08-26 15:01:03 -0300

    Update classify.js

--- Watson Visual Recognition/classify.js
+++ Watson Visual Recognition/classify.js
@@ -5,9 +5,9 @@ const { IamAuthenticator } = require('ibm-watson/auth');
 const visualRecognition = new VisualRecognitionV3({
   version: '2018-03-19',
   authenticator: new IamAuthenticator({
-    apikey: '3ckkLcYlLR-Hwf_vbFnbI1Url1FFABaHupsYeAbYzc-p',
+    apikey: 'API_KEY',
   }),
-  url: 'https://api.us-south.visual-recognition.watson.cloud.ibm.com/instances/10494091-7027-4aed-8cd6-d9d70c422f20',
+  url: 'URL',
 });
 
 const classifyParams = {
